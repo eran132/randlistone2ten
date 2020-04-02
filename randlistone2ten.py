a68a46a3cb32d25f1fc48a82fa21a3280639fd46
Eran <eran132@users.noreply.github.com>
2020-04-02 12:17:11 +0300

    Update randlistone2ten.py

--- randlistone2ten.py
+++ randlistone2ten.py
@@ -1,4 +1,11 @@
+# random module and specifically the shuffle method allows us to change the order of numbers in the range specified
 import random
+
+# Setting a variable equal to a list of numbers in the range of 1 to 10
 num_list = list(range(11))
+
+# Shuffling the order of numbers in the list inside the variable
 random.shuffle(num_list)
+
+# Finally, printing the the list to terminal
 print(num_list)
